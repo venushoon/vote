e00961b912d917d986d61f06c3dab6f17173309e
SHKIM <kimsh1126@gmail.com>
2025-09-05 17:09:22 +0900

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+// src/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FB_DATABASE_URL,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const db = getDatabase(app);
