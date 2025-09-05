3e6324ccf5beec3f764d5bd5eadf28a7855ffe15
SHKIM <kimsh1126@gmail.com>
2025-09-05 20:29:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-{
-  "compilerOptions": {
-    "composite": true,
-    "module": "ESNext",
-    "moduleResolution": "bundler",
-    "types": ["node"]
-  },
-  "include": ["vite.config.ts"]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // ⚠️ GitHub Pages에 올릴 때 반드시 리포 이름과 맞춰주세요.
+  // 예: https://username.github.io/vote → base는 '/vote/'
+  base: '/vote/'
+})
