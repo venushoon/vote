af1df9b91daceab4d7adafdffb0aaea3b633089e
SHKIM <kimsh1126@gmail.com>
2025-09-05 17:25:15 +0900

    Create env.d.ts

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FB_API_KEY: string
+  readonly VITE_FB_AUTH_DOMAIN: string
+  readonly VITE_FB_DATABASE_URL: string
+  readonly VITE_FB_PROJECT_ID: string
+  readonly VITE_FB_STORAGE_BUCKET: string
+  readonly VITE_FB_MESSAGING_SENDER_ID: string
+  readonly VITE_FB_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
